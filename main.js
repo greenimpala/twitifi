24ff502a493580696321d5cba804c99225da48f2
Stephen Bradshaw <stephen.bradshaw@hotmail.co.uk>
2013-03-12 20:34:54 +0000

    Restructure main.js

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-var connect = require('connect');
-var url = require('url');
-var TweetService = require('./lib/TweetService');
-var app = connect();
-var lastRequest = +new Date();
-var IDLE_TIMEOUT = 30 * 1000;
+var Server = require('./lib/Server');
 
-app.use(connect.static('public'));
-app.use(function (req, res) {
-	// Expose '/tweets' endpoint
-	if (/^\/tweets/.test(req.url) && req.method === 'GET') {
-		getTweets(req, res);
-	} else {
-		res.statusCode = 404;
-		res.end();
-	}
-});
-
-function getTweets (req, res) {
-	var sinceId, query;
-
-	lastRequest = +new Date();
-
-	try {
-		query = url.parse(req.url).query;
-		sinceId = parseInt(query.split('=')[1], 10);
-	} catch (e) {}
-
-	if (isNaN(sinceId)) {
-		sinceId = -1;
-	}
-
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(TweetService.getTweetsSinceId(sinceId));
-}
-
-setInterval(function () {
-	var timeSinceLastRequest = +new Date() - lastRequest;
-
-	if (timeSinceLastRequest > IDLE_TIMEOUT && !TweetService.idle) {
-		TweetService.pause();
-	}
-}, IDLE_TIMEOUT);
-
+var app = new Server();
 app.listen(3000, '127.0.0.1');
