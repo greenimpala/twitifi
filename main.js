2a192866cd21648a985495389ae07dc1e14c0f74
Stephen Bradshaw <stephen.bradshaw@hotmail.co.uk>
2013-03-15 21:08:05 +0000

    Use Twitter 1.1 API

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var fs = require('fs');
 var Server = require('./lib/Server');
+var TweetService = require('./lib/TweetService');
 
-var app = new Server();
+var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+var app = new Server(new TweetService(config));
+
 app.listen(3000, '127.0.0.1');
