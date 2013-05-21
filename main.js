d8d665c1a5d9e19de1caa9ddaaac22c793a9d851
Stephen Bradshaw <stephenbradshaw@Stephens-MacBook-Pro.local>
2013-05-21 21:22:40 +0100

    Cleanup start

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var fs = require('fs');
+var http = require('http');
 var Server = require('./lib/Server');
 var TweetService = require('./lib/TweetService');
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
-require('http').globalAgent.maxSockets = 100000;
-var app = new Server(new TweetService(config));
+http.globalAgent.maxSockets = 100000;
+
+var tweetService = new TweetService(config);
+var app = new Server(tweetService);
+
 app.listen(3000, '127.0.0.1');
+console.info('twitifi server started on port 3000.');
