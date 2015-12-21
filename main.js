3617f96274c436707b9e1565dad6d9dcfb597481
Stephen Bradshaw <stephen@bradshaw.io>
2015-12-21 09:53:47 +0000

    read from env

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ var fs = require('fs');
 var http = require('http');
 var Server = require('./lib/Server');
 var TweetService = require('./lib/TweetService');
-var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 var PORT = 3000;
 
 http.globalAgent.maxSockets = 100000;
 
-var tweetService = new TweetService(config);
+var tweetService = new TweetService({
+    "consumer_key": process.env.consumer_key,
+    "consumer_secret": process.env.consumer_secret,
+    "access_token": process.env.access_token,
+    "access_token_secret": process.env.access_token_secret
+});
 var app = new Server(tweetService);
 
 app.listen(PORT, '127.0.0.1');
